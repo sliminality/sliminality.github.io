a4615109da56d136e51e88270c40ff1da530def2
Sarah Lim <sarah@sarahlim.com>
2016-12-22 00:24:19 -0800

    nav menu is fully responsive and has an icon now

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -6,8 +6,10 @@
   // OVERLAY MENU //
   //////////////////
 
-  const toggleNavBtn = $$('site-nav-toggle');
-  const navId = toggleNavBtn.getAttribute('aria-controls');
+  const toggleBtn = $$('site-nav-toggle');
+  const toggleBtnText = $$('site-nav-toggle-text');
+  const toggleBtnIcon = $$('site-nav-toggle-icon');
+  const navId = toggleBtn.getAttribute('aria-controls');
   const nav = $(`#${navId}`);
 
   /**
@@ -21,7 +23,7 @@
   nav.setAttribute('aria-hidden', true);
 
   // Show toggle button.
-  toggleNavBtn.classList.remove(toggleDehydratedClass);
+  toggleBtn.classList.remove(toggleDehydratedClass);
 
   /**
    * Create event handlers for toggling menu state.
@@ -29,19 +31,33 @@
   const toggleMenuVisibility = () => {
     const navActiveClass = 'site-nav__overlay--active';
     const toggleBtnActiveClass = 'site-nav__toggle--active';
+    const toggleBtnIconActiveClass = 'site-nav__toggle-icon--active';
 
     const isActiveBeforeClick = nav.classList.contains(navActiveClass);
 
     nav.classList.toggle(navActiveClass);
     nav.setAttribute('aria-hidden',isActiveBeforeClick);
 
-    toggleNavBtn.classList.toggle(toggleBtnActiveClass);
-    toggleNavBtn.setAttribute('aria-pressed', !isActiveBeforeClick);
+    toggleBtn.classList.toggle(toggleBtnActiveClass);
+    toggleBtn.setAttribute('aria-pressed', !isActiveBeforeClick);
+
+    toggleBtnIcon.classList.toggle(toggleBtnIconActiveClass);
+
+    // Update the text of the button.
+    const inactiveBtnText = 'Menu';
+    const activeBtnText = 'Close';
+
+    if (isActiveBeforeClick) {
+      // Now it is inactive.
+      toggleBtnText.textContent = inactiveBtnText;
+    } else {
+      toggleBtnText.textContent = activeBtnText;
+    }
   };
 
   /**
    * Bind event handler to click activity.
    * TODO: Toggle on media query breakpoints also.
    */
-  toggleNavBtn.addEventListener('click', toggleMenuVisibility);
+  toggleBtn.addEventListener('click', toggleMenuVisibility);
 })();
